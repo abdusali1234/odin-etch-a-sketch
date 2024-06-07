fb13dabf2ef80a8021895b59bfc5ab1f17a3031d
abdusali1234 <abdusali1234@gmail.com>
2024-06-07 10:53:40 +0100

    Added grid sizing and colour change functionality

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const container = document.querySelector(".container");
 const containerLength = 600;
 
+const normalBtn = document.querySelector("#normal");
+const rainbowBtn = document.querySelector("#rainbow");
+const eraseBtn = document.querySelector('#erase');
+const changeSizeBtn = document.querySelector('#change-size');
+
+let paintMode = "normal";
+
 function makeSquareGrid(gridSize) {
   container.textContent = "";
 
@@ -15,9 +22,47 @@ function makeSquareGrid(gridSize) {
     cell.style.outline = "1px solid gray";
     cell.style.height = `${cellLength}px`;
     cell.style.width = `${cellLength}px`;
-    cell.innerText = `${i+1}`;
     container.appendChild(cell);
+
+    cell.addEventListener("mouseover", () => {
+      if (paintMode === "normal"){
+        cell.style.backgroundColor = "black";
+      } else if (paintMode === "erase"){
+        cell.style.backgroundColor = "";
+      } else if (paintMode === "rainbow"){
+        const randR = Math.floor(Math.random()*256);
+        const randG = Math.floor(Math.random()*256);
+        const randB = Math.floor(Math.random()*256); 
+        const randomColor = `rgb(${randR},${randG},${randB})`;
+        cell.style.backgroundColor = randomColor;
+      }
+    } );
   }
 }
 
+normalBtn.addEventListener("click", () => {
+  paintMode = "normal";
+})
+
+rainbowBtn.addEventListener("click", () => {
+  paintMode = "rainbow";
+})
+
+eraseBtn.addEventListener("click", () => {
+  paintMode = "erase";
+})
+
+changeSizeBtn.addEventListener("click", () => {
+
+
+  let userSizeChoice = prompt("Enter a new grid size (1-100): ");
+  if (userSizeChoice > 100 || userSizeChoice < 1){
+    alert("Grid size must be between 1 & 100!");
+  } else {
+    makeSquareGrid(userSizeChoice);
+  }
+})
 makeSquareGrid(16);
+
+
+
